85e91d0c49a2461ead5070e2d5e31332ace99352
yjangda <yujangda@ucsd.edu>
2024-03-12 14:15:24 -0700

    adding initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const port = 3000;
+app.get('/',(req,res)=>{
+    res.send("hello world") 
+});
+app.listen(port,()=>{
+    console.log(`Server Listening on Port http://localhost:${port}`);
+});
