80f5b9b320ab5a2115121d939780de6115996596
yjangda <yujangda@ucsd.edu>
2024-04-04 10:55:27 -0700

    added todo list

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const UserRoute = require("./routers/user.router");
-
+const ToDoRoute = require('./routers/todo.router');
 const app = express();
 
 app.use(bodyParser.json())
 
 app.use("/",UserRoute);
+app.use("/",ToDoRoute);
 
 module.exports = app;
---
+++ routers/todo.router.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const ToDoController = require('../controller/todo.controller');
+
+router.post("/createToDo",ToDoController.createToDo);
+
+router.post('/getUserTodoList',ToDoController.getToDoList)
+
+router.post("/deleteTodo",ToDoController.deleteToDo)
+
+module.exports = router;
